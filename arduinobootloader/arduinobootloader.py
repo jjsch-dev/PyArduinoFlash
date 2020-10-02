27205561ba262ee28234e9234d66cd3eaf543574
jjsch-dev <juanjoseschiavoni@hotmail.com>
2020-10-02 12:07:32 -0300

    first version to send command and receive response for protocol V2

--- arduinobootloader/arduinobootloader.py
+++ arduinobootloader/arduinobootloader.py
@@ -39,9 +39,10 @@ AVR_ATMEL_CPUS = {(0x97, 0x03): ["ATmega1280", (128*2), 512],
                   (0x93, 0x06): ["ATmega8515", (32*2), 128],
                   (0x93, 0x08): ["ATmega8535", (32*2), 128]}
 
-"""STK message constants"""
+"""STK message constants for Stk500v2"""
 MESSAGE_START = 0x1B        # = ESC = 27 decimal
 TOKEN = 0x0E
+CMD_SIGN_ON = 0x01
 
 class ArduinoBootloader(object):
     def __init__(self, *args, **kwargs):
@@ -53,6 +54,7 @@ class ArduinoBootloader(object):
         self._cpu_name = ""
         self._cpu_page_size = 0
         self._cpu_pages = 0
+        self._programmer_name = ""
         self._programmer = None
 
     @property
@@ -109,13 +111,13 @@ class ArduinoBootloader(object):
 
         ''' Clear DTR and RTS to unload the RESET capacitor
             (for example in Arduino) '''
-        self.device.dtr = 0
-        self.device.rts = 0
-        time.sleep(1 / 100)
+        self.device.dtr = True
+        self.device.rts = True
+        time.sleep(1 / 20)
 
         ''' Set DTR and RTS back to high '''
-        self.device.dtr = 1
-        self.device.rts = 1
+        self.device.dtr = False
+        self.device.rts = False
         time.sleep(1 / 20)
 
         return True
@@ -127,16 +129,24 @@ class ArduinoBootloader(object):
             self.device = None
 
     class Stk500v1(object):
+        """It encapsulates the communication protocol that Arduino uses for the first
+           versions of bootoloader, which can write up to 128 K bytes of flash memory.
+           For example: Nano, Uno, etc"""
         def __init__(self, ab):
             self._ab = ab
             self._answer = None
 
         def open(self, port=None, speed=57600):
+            """Find and open the communication port where the Arduino is connected.
+               Generate the reset sequence with the DTR / RTS pins.
+               Send the sync command to verify that there is a valid bootloader."""
             if self._ab.open(port, speed):
                 return self.get_sync()
 
             return False
+
         def close(self):
+            """Close the communication port."""
             self._ab.close()
 
         def get_sync(self):
@@ -145,27 +155,27 @@ class ArduinoBootloader(object):
             """
             self._ab.device.timeout = 1 / 5
             for i in range(1, 2):
-                self.cmd_request(b"0 ", answer_len=2)
+                self._cmd_request(b"0 ", answer_len=2)
 
             self._ab.device.timeout = 1
             for i in range(1, 3):
-                if self.cmd_request(b"0 ", answer_len=2):
+                if self._cmd_request(b"0 ", answer_len=2):
                     return True
             return False
 
         def board_request(self):
             """Get the firmware and hardware version of the bootloader."""
-            if not self.cmd_request(b"A\x80 ", answer_len=3):
+            if not self._cmd_request(b"A\x80 ", answer_len=3):
                 return False
 
             self._ab._hw_version = self._answer[1]
 
-            if not self.cmd_request(b"A\x81 ", answer_len=3):
+            if not self._cmd_request(b"A\x81 ", answer_len=3):
                 return False
 
             self._ab._hw_version = self._answer[1]
 
-            if not self.cmd_request(b"A\x82 ", answer_len=3):
+            if not self._cmd_request(b"A\x82 ", answer_len=3):
                 return False
 
             self._ab._sw_minor = self._answer[1]
@@ -174,7 +184,7 @@ class ArduinoBootloader(object):
 
         def cpu_signature(self):
             """Get information about the CPU, name and size of the flash memory page"""
-            if self.cmd_request(b"u ", answer_len=5):
+            if self._cmd_request(b"u ", answer_len=5):
                 if self._answer[1] == SIG1_ATMEL:
                     try:
                         list_cpu = AVR_ATMEL_CPUS[(self._answer[2], self._answer[3])]
@@ -186,16 +196,6 @@ class ArduinoBootloader(object):
                         self._ab._cpu_name = "SIG2: {:02x} SIG3: {:02x}".format(self._answer[2], self._answer[3])
                         self._ab._cpu_page_size = 0
                         self._ab._cpu_pages = 0
-            return False
-
-        def cmd_request(self, msg, answer_len):
-            if self._ab.device:
-                self._ab.device.write(msg)
-                self._answer = self._ab.device.read(answer_len)
-
-                if len(self._answer) == answer_len and \
-                        self._answer[0] == RESP_STK_IN_SYNC and self._answer[answer_len - 1] == RESP_STK_OK:
-                    return True
             return False
 
         def write_memory(self, buffer, address, flash=True):
@@ -213,7 +213,7 @@ class ArduinoBootloader(object):
                 cmd.extend(buffer)
                 cmd.append(ord(' '))
 
-                return self.cmd_request(cmd, answer_len=2)
+                return self._cmd_request(cmd, answer_len=2)
             return False
 
         def read_memory(self, address, count, flash=True):
@@ -227,7 +227,7 @@ class ArduinoBootloader(object):
                 cmd[3] = ord('F') if flash else ord('E')
                 cmd[4] = ord(' ')
 
-                if self.cmd_request(cmd, answer_len=count+2):
+                if self._cmd_request(cmd, answer_len=count+2):
                     # The answer start with RESP_STK_IN_SYNC and finish with RESP_STK_OK
                     buffer = bytearray(self._answer[1:count+1])
 
@@ -246,43 +246,106 @@ class ArduinoBootloader(object):
             cmd[2] = ((address >> 8) & 0xFF)
             cmd[3] = ord(' ')
 
-            return self.cmd_request(cmd, answer_len=2)
+            return self._cmd_request(cmd, answer_len=2)
 
         def leave_prg_mode(self):
             """Tells the bootloader to leave programming mode and start executing the stored firmware"""
-            return self.cmd_request(b"Q ", answer_len=2)
+            return self._cmd_request(b"Q ", answer_len=2)
 
-    class Stk500V2(object):
-        def __init__(self, *args, **kwargs):
-            _answer = None
-            _sequence_number = 0
+        def _cmd_request(self, msg, answer_len):
+            """send and receive a command in stk500v1 format"""
+            if self._ab.device:
+                self._ab.device.write(msg)
+                self._answer = self._ab.device.read(answer_len)
 
-        def _send_comand(self, cmd, data, bytes_count):
-            buff = bytearray(5)
-            checksum = 0
+                if len(self._answer) == answer_len and \
+                        self._answer[0] == RESP_STK_IN_SYNC and self._answer[answer_len - 1] == RESP_STK_OK:
+                    return True
+            return False
 
-            buff[0] = MESSAGE_START
-            buff[1] = _sequence_number
-            buff[2] = ((bytes_count >> 8) & 0xFF)
-            buff[3] = (bytes_count & 0xFF)
-            buff[4] = TOKEN
-            buff.extend(data)
+    class Stk500v2(object):
+        def __init__(self, ab):
+            self._ab = ab
+            self._answer = None
+            self._sequence_number = 0
 
-            for val in buff:
-                checksum ^= val
+        def open(self, port=None, speed=115200):
+            """Find and open the communication port where the Arduino is connected.
+               Generate the reset sequence with the DTR / RTS pins.
+               Send the sync command to verify that there is a valid bootloader."""
+            if self._ab.open(port, speed):
+                return self.get_sync()
 
-        def _read_answer(self):
-            buff = bytearray(5)
-            checksum = 0
+            return False
 
-            buff[0] = MESSAGE_START
-            buff[1] = _sequence_number
-            buff[2] = ((bytes_count >> 8) & 0xFF)
-            buff[3] = (bytes_count & 0xFF)
-            buff[4] = TOKEN
-            buff.extend(data)
+        def close(self):
+            """Close the communication port."""
+            self._ab.close()
 
-            for val in buff:
-                checksum ^= val
+        def get_sync(self):
+            if self._send_command(CMD_SIGN_ON):
+                if self._recv_answer(CMD_SIGN_ON):
+                    prog_name_len = self._answer[0]
+                    del self._answer[0]
 
-            buff.append(checksum)
+                    self._ab._programmer_name = self._answer.decode("utf-8")
+                    return True
+            return False
+
+        def _send_command(self, cmd, data=None):
+            if self._ab.device:
+                self._sequence_number += 1
+
+                buff = bytearray(5)
+                checksum = 0
+                data_len = 1 if data is None else len(dat) + 1
+
+                buff[0] = MESSAGE_START
+                buff[1] = self._sequence_number
+                buff[2] = ((data_len >> 8) & 0xFF)
+                buff[3] = (data_len & 0xFF)
+                buff[4] = TOKEN
+                buff.append(cmd)
+                if not data is None:
+                    buff.extend(data)
+
+                for val in buff:
+                    checksum ^= val
+
+                buff.append(checksum)
+
+                self._ab.device.write(buff)
+                return True
+            return False
+
+        def _recv_answer(self, cmd):
+            """the response is composed of a fixed size header, and the first two
+            bytes of the data contain the command and the status of the operation."""
+            head = bytearray(self._ab.device.read(5))
+
+            """For the header to be valid, the beginning and end, plus the sequence number, must match."""
+            if len(head) == 5 and head[0] == MESSAGE_START and\
+                head[4] == TOKEN and self._sequence_number == head[1]:
+
+                """one is added because the length does not include the checksum byte"""
+                len_data = ((head[2] << 8) | head[3]) + 1
+
+                """The minimum response contains the command and the status of the operation."""
+                if len_data >= 3:
+                    self._answer = bytearray(self._ab.device.read(len_data))
+                    if len(self._answer) == len_data and\
+                        self._answer[0] == cmd and self._answer[1] == 0:
+                        answ_chk = self._answer[-1]
+                        del self._answer[-1]
+
+                        head.extend(self._answer)
+                        checksum = 0
+                        for val in head:
+                            checksum ^= val
+                        """answer now contains the bytes of the data."""
+                        del self._answer[0]
+                        del self._answer[0]
+
+                        """if the calculation matches the checksum, the answer is valid."""
+                        return checksum == answ_chk
+                return False
