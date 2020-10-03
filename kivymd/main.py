c418aeffc4eedca9e1e9d7137ea45d8ab50263cd
jjsch-dev <juanjoseschiavoni@hotmail.com>
2020-10-03 15:16:20 -0300

    The APP in Kivy was updated to support the selection of Stk500v1 and v2 protocols

--- kivymd/main.py
+++ kivymd/main.py
@@ -10,7 +10,7 @@ from queue import Queue
 
 from intelhex import IntelHex
 from intelhex import AddressOverlapError
-from arduinobootloader.arduinobootloader import ArduinoBootloader
+from arduinobootloader import ArduinoBootloader
 
 
 KV = '''
@@ -40,6 +40,17 @@ Screen:
                 text:"Bootloader hardware version"
             MDLabel:
                 id:hw_version
+                text:"--"
+        
+        MDBoxLayout:
+            orientation:"horizontal"
+            padding:10
+            spacing:10
+            
+            MDLabel:
+                text:"Bootloader name"
+            MDLabel:
+                id:prg_name
                 text:"--"
        
         MDBoxLayout:
@@ -74,8 +85,37 @@ Screen:
                 helper_text: "Please enter the file and path of the Arduino firmware"
                 helper_text_mode: "on_focus"
                 text:"test.hex"
-                        
-        BoxLayout:
+        
+        MDBoxLayout:
+            orientation: "horizontal"
+            ScrollView:
+                MDList:
+                                     
+                    ThreeLineAvatarListItem:
+                        on_release: app.on_sel_programmer(115200, "Stk500v1")  
+                        text: "Protocol arduino or STK500V1"
+                        secondary_text: "for boards up to 128 Kbytes"
+                        tertiary_text: "example Nano or Uno - baudarate 115200"
+                        ImageLeftWidget:
+                            source: "images/arduino-nano.png"
+                    
+                    ThreeLineAvatarListItem:
+                        on_release: app.on_sel_programmer(57600, "Stk500v1") 
+                        text: "Protocol arduino or STK500V1 - older"
+                        secondary_text: "for boards up to 128 Kbytes"
+                        tertiary_text: "example Nano or Uno - baudarate 57600"  
+                        ImageLeftWidget:
+                            source: "images/arduino-uno.png" 
+        
+                    ThreeLineAvatarListItem:
+                        on_release: app.on_sel_programmer(115200, "Stk500v2") 
+                        text: "Protocol wiring or STK500V2"
+                        secondary_text: "for boards with more than 128 Kbytes"
+                        tertiary_text: "example Mega - baudarate 115200"
+                        ImageLeftWidget:
+                            source: "images/arduino-mega-2560-original.png"
+                                                                    
+        MDBoxLayout:
             padding: "10dp"
             orientation: "horizontal"
             MDProgressBar:
@@ -84,7 +124,7 @@ Screen:
                 min: 0
                 max: 1
         
-        BoxLayout:
+        MDBoxLayout:
             padding: "10dp"
             orientation: "horizontal"    
             MDLabel:
@@ -105,9 +145,15 @@ class MainApp(MDApp):
         self.ab = ArduinoBootloader()
         self.working_thread = None
         self.progress_queue = Queue(100)
+        self.protocol = "Stk500v1"
+        self.baudrate = 115200
 
     def build(self):
         return Builder.load_string(KV)
+
+    def on_sel_programmer(self, baudrate, protocol):
+        self.baudrate = baudrate
+        self.protocol = protocol
 
     def on_flash(self):
         del self.ih
@@ -135,12 +181,20 @@ class MainApp(MDApp):
            established, obtains the information of the processor and the bootloader."""
         res_val = False
 
-        if self.ab.open():
-            if self.ab.board_request():
+        """First you have to select the communication protocol used by the bootloader of 
+        the Arduino board. The Stk500V1 is the one used by the Nano or Uno, and depending 
+        on the Old or New version, the communication speed varies, for the new one you 
+        have to use 115200 and for the old 57600.
+        
+        The communication protocol for boards based on Mega 2560 is Stk500v2 at 115200."""
+        prg = self.ab.select_programmer(self.protocol)
+
+        if prg.open(speed=self.baudrate):
+            if prg.board_request():
                 self.progress_queue.put(["board_request"])
                 Clock.schedule_once(self.progress_callback, 1 / 1000)
 
-            if self.ab.cpu_signature():
+            if prg.cpu_signature():
                 self.progress_queue.put(["cpu_signature"])
                 Clock.schedule_once(self.progress_callback, 1 / 1000)
 
@@ -148,7 +202,7 @@ class MainApp(MDApp):
                use the write flash command to update the cpu."""
             for address in range(0, self.ih.maxaddr(), self.ab.cpu_page_size):
                 buffer = self.ih.tobinarray(start=address, size=self.ab.cpu_page_size)
-                res_val = self.ab.write_memory(buffer, address)
+                res_val = prg.write_memory(buffer, address)
                 if not res_val:
                     break
 
@@ -160,7 +214,7 @@ class MainApp(MDApp):
             if res_val:
                 for address in range(0, self.ih.maxaddr(), self.ab.cpu_page_size):
                     buffer = self.ih.tobinarray(start=address, size=self.ab.cpu_page_size)
-                    read_buffer = self.ab.read_memory(address, self.ab.cpu_page_size)
+                    read_buffer = prg.read_memory(address, self.ab.cpu_page_size)
                     if not len(read_buffer) or (buffer != read_buffer):
                         res_val = False
                         break
@@ -171,18 +225,25 @@ class MainApp(MDApp):
             self.progress_queue.put(["result", "ok" if res_val else "error", address])
             Clock.schedule_once(self.progress_callback, 1 / 1000)
 
-        self.ab.leave_prg_mode()
+            prg.leave_bootloader()
 
-        self.ab.close()
+            prg.close()
+        else:
+            self.progress_queue.put(["open_error"])
+            Clock.schedule_once(self.progress_callback, 1 / 1000)
 
     def progress_callback(self, dt):
         """In kivy only the main thread can update the widgets. Schedule a clock
            event to read the message from the queue and update the progress."""
         value = self.progress_queue.get()
 
+        if value[0] == "open_error":
+            self.root.ids.status.text = "Can't open bootloader {} at baudrate {}".format(self.protocol, self.baudrate)
+
         if value[0] == "board_request":
             self.root.ids.sw_version.text = self.ab.sw_version
             self.root.ids.hw_version.text = self.ab.hw_version
+            self.root.ids.prg_name.text = self.ab.programmer_name
 
         if value[0] == "cpu_signature":
             self.root.ids.cpu_version.text = self.ab.cpu_name
